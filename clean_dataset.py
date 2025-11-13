a8a03d2621fd37493e9e40d53d20cb610dc2b60c
Dopa <guengant.noe@gmail.com>
2025-11-13 10:24:58 +0100

    fix: pep8 friendly

--- clean_dataset.py
+++ clean_dataset.py
@@ -1,31 +1,31 @@
-
 import pandas as pd
 
-def outliers_clean():
-    "This script aim to clean ouliers from the dataset"
 
-    # Loading dataset
-    df = pd.read_csv("./plane_ticket_price_cleaned.csv")
+def outliers_clean(df: pd.DataFrame) -> pd.DataFrame:
+    """This script aim to clean ouliers from the dataset"""
 
     # Counting outliers values changed
     counter = 0
 
     for col in df.select_dtypes(include=['float64', 'int64']):
-        Q1 = df[col].quantile(0.25)
-        Q3 = df[col].quantile(0.75)
-        IQR = Q3 - Q1
-        borne_min = Q1 - 1.5 * IQR
-        borne_max = Q3 + 1.5 * IQR
+        q1 = df[col].quantile(0.25)
+        q3 = df[col].quantile(0.75)
+        iqr = q3 - q1
+        borne_min = q1 - 1.5 * iqr
+        borne_max = q3 + 1.5 * iqr
         outliers = df[(df[col] < borne_min) | (df[col] > borne_max)]
         counter += 1
         print(f"{col} : {len(outliers)} valeurs aberrantes")
         print(f"{counter} outliers changed")
 
-def fill_nul():
-    "This script aims to fill empty values in the dataset"
+    return df
+
+
+def fill_nul(path: str) -> pd.DataFrame:
+    """This script aims to fill empty values in the dataset"""
 
     # Loading dataset
-    df = pd.read_csv("./plane_ticket_price_cleaned.csv")
+    df = pd.read_csv(path)
 
     # Filling empty number values
     for col in df.select_dtypes(include=['float64', 'int64']):
@@ -39,6 +39,8 @@ def fill_nul():
         df[col].fillna(mode)
         print(f"Add value {mode} at {col}")
 
+    return df
+
+
 if __name__ == "__main__":
-    fill_nul()
-    outliers_clean()
+    outliers_clean(fill_nul("./plane_ticket_price_original.csv"))
